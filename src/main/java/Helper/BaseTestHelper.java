595e80aea052f952f9a2ae5ed6c6974ab3def9aa
RajatManga <RajatManga1992@gmail.com>
2024-09-19 16:24:25 +0530

    Updated Test Cases Report integrated

---
+++ src/main/java/Helper/BaseTestHelper.java
@@ -0,0 +1,21 @@
+package Helper;
+
+import java.io.File;
+import java.util.Date;
+
+public class BaseTestHelper {
+
+    public static  void Createfolder(String path){
+        File file = new File(path);
+        if(!file.exists()){
+            file.mkdir();
+        }else {
+            System.out.println("Folder already created");
+        }
+    }
+    public static String Timestamp(){
+        Date now = new Date();
+        String Timestamp = now.toString().replace(":","-");
+        return Timestamp;
+    }
+}
